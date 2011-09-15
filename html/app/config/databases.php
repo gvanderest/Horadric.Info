4643ba7ecc20f199350af9dc12b369c269b884dc
Guillaume VanderEst <guillaume@vanderest.org>
2011-09-15 16:21:12 -0700

    Removed live database info

--- html/app/config/databases.php
+++ html/app/config/databases.php
@@ -5,13 +5,6 @@
 
 $databases['default'] = array(
     'engine' => 'mysql',
-    'host' => 'internal-db.s118123.gridserver.com',
-    'username' => 'db118123_orm',
-    'password' => 'password',
-    'database' => 'db118123_orm'
-);
-$databases['default'] = array(
-    'engine' => 'mysql',
     'host' => 'localhost',
     'username' => 'root',
     'password' => '',
